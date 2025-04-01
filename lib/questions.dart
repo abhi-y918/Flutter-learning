775fb01d0210b75c606c0cad2b3965ce717d3f85
abhi-y918 <abhinavy918@gmail.com>
2025-04-02 02:09:02 +0530

    RETURNED AFTER LONG TIME

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:adv_basics/Answer_button.dart';
+import 'package:adv_basics/data1/questions.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});//constructor function
@@ -9,29 +11,33 @@ class QuestionsScreen extends StatefulWidget{
   }
 }
 class _QuestionsScreenState extends State<QuestionsScreen>{
+  double get value => 0.0;
+
   @override
   Widget build( context) {
+    final currentQuestion= questions[0];
 
    return SizedBox(
    width: double.infinity,
-   child :Column(
+   child:Container(
+        margin: const EdgeInsets.all(40),
+     child :Column(
      mainAxisAlignment: MainAxisAlignment.center,
+     crossAxisAlignment: CrossAxisAlignment.stretch,
      children: [
-     const Text('The Question...'),
+      Text(
+        currentQuestion.text,
+       style: TextStyle(
+         color: Colors.white,
+       ),
+        textAlign: TextAlign.center,
+   ),
      const SizedBox(height: 30,),
-     ElevatedButton(
-       onPressed: (){},
-         child: Text('Answer 1'),
-     ),
-     ElevatedButton(
-       onPressed: (){},
-       child: Text('Answer 2'),
-     ),
-     ElevatedButton(
-       onPressed: (){},
-       child: Text('Answer 3'),
-     ),
-   ],
+    ...currentQuestion.answers.map((answer){//these dots are used for spreading values
+      return AnswerButton(answerText: answer, onTap: (){});
+    })
+     ],
+   ),
    ),
    );
   }
